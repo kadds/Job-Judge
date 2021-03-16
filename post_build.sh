859b0bdc19236c4d48fdba8158d2822e56f1a0f4
kadds <itmyxyf@gmail.com>
2021-03-16 23:26:38 +0800

    try tar artifact

--- post_build.sh
+++ post_build.sh
@@ -4,7 +4,6 @@ function copy_data(){
     strip -s target/x86_64-unknown-linux-musl/release/${1} -o out/${1}/${1}
     upx -9 out/${1}/${1}
     sed "s/\${{BINARY}}/${1}/g" server/Dockerfile.template > out/${1}/Dockerfile
-    chmod +x out/${1}/${1}
 }
 copy_data 'usersvr'
 copy_data 'sdwp'
