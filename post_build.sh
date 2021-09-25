a8890c040ab2ec523146f942b45a6a4756e5027d
kadds <itmyxyf@gmail.com>
2021-09-26 01:00:02 +0800

    ci update

--- post_build.sh
+++ post_build.sh
@@ -5,8 +5,18 @@ function copy_data(){
     upx -9 out/${1}/${1}
     sed "s/\${{BINARY}}/${1}/g" server/Dockerfile.template > out/${1}/Dockerfile
 }
+
+function copy_sdwp(){
+    mkdir -p out/${1}/static
+    strip -s target/x86_64-unknown-linux-musl/release/${1} -o out/${1}/${1}
+    upx -9 out/${1}/${1}
+    sed "s/\${{BINARY}}/${1}/g" server/Dockerfile.template > out/${1}/Dockerfile
+    cp -r server/sdwp/web/dist/* out/${1}/static/
+}
+
 copy_data 'usersvr'
-copy_data 'sdwp'
 copy_data 'sessionsvr'
 copy_data 'gateway'
 copy_data 'idsvr'
+copy_data 'testsvr'
+copy_sdwp 'sdwp'
